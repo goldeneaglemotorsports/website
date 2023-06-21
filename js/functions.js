06bbcb7ed11cbe81dfe78b2fc5f0a9bf7d183250
Salman Sheikh <SalmanSheikh2180@gmail.com>
2023-06-21 00:34:20 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,55 @@
 
-const buttons = document.querySelector("[data-carousel-button]")
+// const buttons = document.querySelector("[data-carousel-button]")
 
-buttons.forEach(button => {
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         const offset = button.dataset.carouselButton === "next" ? 1 : -1
+//         const slides = button
+//             .closest("[data-carousel]")
+//             .querySelector('[data-slides]')
+
+//         const activeSlide = slides.querySelector("[data-active]")
+//         let newIndex = [...slides.children].indexOf(activeSlide) + offset
+//         if (newIndex < 0) newIndex = slides.children.length - 1
+//         if (newIndex >= slides.children.length) newIndex = 0
+
+//         slides.children[newIndex].dataset.active = true
+//         delete activeSlide.dataset.active
+
+
+
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+Array.prototype.forEach.call(buttons, (button) => {
     button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
         const slides = button
             .closest("[data-carousel]")
-            .querySelector('[data-slides]')
+            .querySelector('[data-slides]');
 
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = Array.from(slides.children).indexOf(activeSlide) + offset;
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
 
-
-
+        // Remove the active state from the currently active slide
+        activeSlide.dataset.active = "false";
+        
+        // Add the active state to the new active slide
+        slides.children[newIndex].dataset.active = "true";
     })
 })
