b2d38be43f2b71ea788a81dc7ff79d9c4eb23e1f
Salman Sheikh <SalmanSheikh2180@gmail.com>
2023-06-21 00:32:24 -0700

    update

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,22 @@
-// function isInViewport(el) {
-//
-//     const rect = el.getBoundingClientRect();
-//     // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
-//     const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-//     const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-//
-//     // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
-//     const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-//     const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
-//
-//     return (vertInView && horInView);
-// }
-//
-//
-// function changeButtonBackground(element, element2, button) {
-//       if (isInViewport(element) && !isInViewport(element2)) {
-//         button.style.backgroundColor = "#999900";
-//       }
-//       else {
-//         button.style.backgroundColor = "#000000";
-//       }
-// }
-//
-// var events = document.getElementById('eventssection');
-// var signup = document.getElementById('signup');
-// var board = document.getElementById('officers');
-// var contactUs = document.getElementById('contactUsSection');
-// var eventsButton = document.getElementById('eventsButton');
-// var signupButton = document.getElementById('signupButton');
-// var boardButton = document.getElementById('boardButton');
-// var contactUsButton = document.getElementById('contactUsButton');
-//
-// window.addEventListener('scroll', function(event) {
-//   changeButtonBackground(events, signup, eventsButton);
-//   changeButtonBackground(signup, board, signupButton);
-//   changeButtonBackground(board, contactUs, boardButton);
-//   changeButtonBackground(contactUs, events, contactUsButton);
-// }, false);
+
+const buttons = document.querySelector("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector('[data-slides]')
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+
+
+
+    })
+})
